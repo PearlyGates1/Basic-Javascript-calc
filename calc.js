6016021aff6f459a2599a992c2d543795be1398c
Pearl Okoroafor <pearlygatess01@gmail.com>
2022-06-22 18:03:50 +0100

    Initial commit

---
+++ calc.js
@@ -0,0 +1,27 @@
+
+    let operator = prompt("Enter operator(e.g + - / *):");
+    let FirstNumber = parseFloat(prompt("Enter first number: "));
+    let SecondNumber = parseFloat(prompt("Enter second number: "));
+
+    let result = null;
+    if (isNaN(FirstNumber) && isNaN(SecondNumber)) {
+        alert("Invalid Input")
+        calculator();
+    }
+
+    else if (operator == '+') {
+        result = FirstNumber + SecondNumber;
+    }
+    else if (operator == '-') {
+        result = FirstNumber - SecondNumber;
+    }
+    else if (operator == '/') {
+        result = FirstNumber / SecondNumber;
+    }
+    else if (operator == '*') {
+        result = FirstNumber * SecondNumber;
+    }
+
+    alert('Result is ' + result);
+
+
